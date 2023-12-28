0934532c4f7152823ec46af2e8f38744ce8a84ee
Yeonjin Shin <yeonjin@yeonjin.name>
2023-12-28 11:37:14 +0900

    Complete impl category button behaivor

--- Main/dist/assets/js/top_button_nav.js
+++ Main/dist/assets/js/top_button_nav.js
@@ -51,6 +51,27 @@ function getIndexOfCategoryButtonKey(key) {
 function categoryKeyIndexToKey(keyIdx) {
     return categoryButtonData[keyIdx].key;
 }
+/**
+ * 해당 key를 가진 카테고리 버튼들 중 현재 활성화된 버튼과 가장 가까운 버튼을 반환한다.
+ * @param {string} key 카테고리 버튼 키
+ * @returns 카테고리 버튼 Element object
+ */
+function getCategoryButtonByKey(key) {
+    const buttons = [
+        ...document.querySelectorAll("nav.top-category-buttons > button"),
+    ];
+    const buttonsWithDistance = buttons
+        .filter((i) => i.dataset.key === key)
+        .map((i) => ({
+            button: i,
+            distance: Math.abs(
+                buttons.indexOf(i) - buttons.indexOf(getActiveCategoryButton())
+            ),
+        }))
+        .sort((a, b) => a.distance - b.distance);
+
+    return buttonsWithDistance[0].button;
+}
 
 // x를 [0, max) 내의 정수로 정규화한다.
 function normalizeIntoRange(x, maxExcluding) {
@@ -67,7 +88,7 @@ function getActiveCategoryButton() {
 [...document.querySelectorAll("nav.top-category-buttons > button")].forEach(
     (i) => i.addEventListener("click", handleCategoryButtonNavClick)
 );
-function handleCategoryButtonNavClick(evt) {
+function prepareChangeActiveButton(target) {
     const activeNow = getActiveCategoryButton();
 
     // nav 너비 계산
@@ -86,7 +107,7 @@ function handleCategoryButtonNavClick(evt) {
     const buttons = [
         ...document.querySelectorAll("nav.top-category-buttons > button"),
     ];
-    const leftButtonCount = buttons.indexOf(evt.target);
+    const leftButtonCount = buttons.indexOf(target);
     const rightButtonCount = buttons.length - leftButtonCount - 1;
 
     // 추가하거나 제거할 버튼 개수 계산
@@ -139,9 +160,55 @@ function handleCategoryButtonNavClick(evt) {
                 )
             );
     }
+}
+
+function changeActiveButton(target) {
+    // 스크롤링
+    scrollCategoryButtonToCenter(target);
+    getActiveCategoryButton().classList.remove("active");
+    target.classList.add("active");
+}
+
+function handleCategoryButtonNavClick(evt) {
+    // post-board-column -- 카테고리 버튼 동기화 잠시 비활성화
+    isCategoryButtonColumnSynchronizationEnabled = false;
+
+    // 필요한 버튼 생성
+    prepareChangeActiveButton(evt.target);
+
+    // Column 이동
+    const targetColumn = [
+        ...document.querySelectorAll("article.column"),
+    ].filter((i) => i.dataset.key === evt.target.dataset.key)[0];
+    const currentlyShownColumn = getMostShownPostBoardColumn().target;
+    const buttons = [
+        ...document.querySelectorAll("nav.top-category-buttons > button"),
+    ];
+    if (
+        buttons.indexOf(getActiveCategoryButton()) < buttons.indexOf(evt.target)
+    ) {
+        document
+            .querySelector(".post-board-columns")
+            .insertBefore(
+                targetColumn,
+                getMostShownPostBoardColumn().target.nextSibling
+            );
+        scrollPostBoardColumnIntoView(currentlyShownColumn, false);
+    } else if (
+        buttons.indexOf(getActiveCategoryButton()) > buttons.indexOf(evt.target)
+    ) {
+        document
+            .querySelector(".post-board-columns")
+            .insertBefore(targetColumn, getMostShownPostBoardColumn().target);
+        scrollPostBoardColumnIntoView(currentlyShownColumn, false);
+    }
+
+    // Column 전환
+    scrollPostBoardColumnIntoView(targetColumn);
 
     // 스크롤링
-    scrollCategoryButtonToCenter(evt.target);
-    getActiveCategoryButton().classList.remove("active");
-    evt.target.classList.add("active");
+    changeActiveButton(evt.target);
+
+    // 동기화 활성화
+    isCategoryButtonColumnSynchronizationEnabled = true;
 }
