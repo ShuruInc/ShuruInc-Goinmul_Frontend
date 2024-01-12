ca98e6842273861c87b82d7f4aecd50a58b80934
Yeonjin Shin <yeonjin@yeonjin.name>
2024-01-12 11:21:35 +0900

    fix: do not quit when exiting non-nerd test page

--- Main/src/page/quiz/solve.ts
+++ Main/src/page/quiz/solve.ts
@@ -4,6 +4,7 @@ import { QuizApiClient } from "../../api/quiz";
 import { InitTopNav } from "../../top_bottom_animation";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import initSolvePage from "../../solvePage";
+import { QuizSession } from "../../api/quiz_session";
 
 InitTopNav();
 
@@ -13,34 +14,51 @@ dom.i2svg({ node: document.querySelector("article.loading")! });
 
 const params = new URLSearchParams(location.search.substring(1));
 const quizId = params.get("id");
-if (quizId === null || quizId === " ") {
-    alert("오류");
-} else {
-    QuizApiClient.isNerdTest(quizId).then((isNerdTest) => {
-        if (isNerdTest) {
-            document
-                .querySelector("article.loading")
-                ?.classList.add("display-none");
-            document
-                .querySelector("article.display-none")
-                ?.classList.remove("display-none");
-            document
-                .querySelector("form")
-                ?.addEventListener("submit", (evt) => {
-                    evt.preventDefault();
-                    QuizApiClient.startNerdQuiz(quizId, {
-                        nickname: (
-                            document.querySelector(
-                                "#nickname"
-                            ) as HTMLInputElement
-                        ).value,
-                        email: (
-                            document.querySelector("#email") as HTMLInputElement
-                        ).value,
-                    }).then(initSolvePage);
-                });
+const sessionId = params.get("session");
+const initByQuizId = () => {
+    if (quizId !== null) {
+        QuizApiClient.isNerdTest(quizId).then((isNerdTest) => {
+            if (isNerdTest) {
+                document
+                    .querySelector("article.loading")
+                    ?.classList.add("display-none");
+                document
+                    .querySelector("article.display-none")
+                    ?.classList.remove("display-none");
+                document
+                    .querySelector("form")
+                    ?.addEventListener("submit", (evt) => {
+                        evt.preventDefault();
+                        QuizApiClient.startNerdQuiz(quizId, {
+                            nickname: (
+                                document.querySelector(
+                                    "#nickname"
+                                ) as HTMLInputElement
+                            ).value,
+                            email: (
+                                document.querySelector(
+                                    "#email"
+                                ) as HTMLInputElement
+                            ).value,
+                        }).then(initSolvePage);
+                    });
+            } else {
+                QuizApiClient.startQuiz(quizId).then(initSolvePage);
+            }
+        });
+    } else {
+        alert("오류가 발생했습니다.");
+    }
+};
+if (sessionId !== null) {
+    const session = new QuizSession(sessionId);
+    session.sessionInfo().then((info) => {
+        if (info.isNerdTest) {
+            initByQuizId();
         } else {
-            QuizApiClient.startQuiz(quizId).then(initSolvePage);
+            initSolvePage(session);
         }
     });
+} else {
+    initByQuizId();
 }
