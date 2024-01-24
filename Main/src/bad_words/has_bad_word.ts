2aa3f97539c84d15548849c8b7af95d93c63b75e
Yeonjin Shin <yeonjin@yeonjin.name>
2024-01-24 16:38:46 +0900

    bad_word: ban -충 words

--- Main/src/bad_words/has_bad_word.ts
+++ Main/src/bad_words/has_bad_word.ts
@@ -1,6 +1,7 @@
 import badWordData from "./data";
 
 export default function hasBadWord(str: string): string | false {
+    if (str.length >= 2 && str.endsWith("충")) return "-충";
     for (const i of badWordData) {
         if (new RegExp(i.trim(), "g").test(str)) {
             return i;
