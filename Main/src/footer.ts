19916b591c5b303dacd0bbc557b3452aa7f10320
Yeonjin Shin <yeonjin@yeonjin.name>
2024-02-07 10:32:38 +0900

    feat: add twitter link into footer

--- Main/src/footer.ts
+++ Main/src/footer.ts
@@ -1,5 +1,12 @@
+import { icon } from "@fortawesome/fontawesome-svg-core";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
 export default function footer() {
     const footer = document.createElement("footer");
-    footer.innerHTML = "Copyright (C) 2024 Shuru";
+    footer.innerHTML = `Copyright (C) 2024 Shuru<br>
+        <div class="icons">
+            <a class="twitter" href="https://twitter.com/messages/compose?recipient_id=1175998448841056256"></a>
+        </div>`;
+    footer.querySelector("a.twitter")!.innerHTML = icon(faTwitter).html[0];
     return footer;
 }
