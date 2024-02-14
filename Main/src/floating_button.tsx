95baeb8d808833bffc1a7d597a894e5c19bf2a0d
Yeonjin Shin <yeonjin@yeonjin.name>
2024-02-14 16:25:19 +0900

    port(to_react): rewrite FloatingButton into react component

---
+++ Main/src/floating_button.tsx
@@ -0,0 +1,19 @@
+import { faChevronUp, faHome } from "@fortawesome/free-solid-svg-icons";
+import "../styles/floating-button.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from '../styles/floating_button.module.scss';
+
+type FloatingButtonIconType = "up" | "home";
+
+type FloatingButtonProp = Partial<{
+    type: FloatingButtonIconType,
+    onClick: FloatingButtonListener;
+}>;
+
+type FloatingButtonListener = () => void;
+
+export default function FloatingButton({type, onClick}: FloatingButtonProp) {
+    return <button className={styles.floatingBtn} onClick={onClick}>
+        <FontAwesomeIcon icon={type === 'up' ? faChevronUp : faHome}></FontAwesomeIcon> 
+    </button>;
+}
