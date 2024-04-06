f0c9631d45352bcfa762724fb263b9b6d75e154d
Cocoa Hoto <cocoa@hoto.us>
2024-04-07 00:40:44 +0900

    fix: 스크롤 로직 수정

--- Main/src/solve_page.ts
+++ Main/src/solve_page.ts
@@ -325,9 +325,6 @@ export default function initSolvePage(session: QuizSession) {
     
     if (topbar === null) return;
 
-    topbar.style.background = 'inherit';
-    topbar.style.backgroundPositionX = '-55px';
-
     window.addEventListener('scroll', function() {
         const position = (25 - window.scrollY) % 75;
         topbar.style.backgroundPositionY = `${position}px`;
