49268ae51285190b9111338c6c58bdaa359ec821
Cocoa Hoto <cocoa@hoto.us>
2024-04-07 00:38:31 +0900

    fix: 스크롤 로직 수정

--- Main/src/solve_page.ts
+++ Main/src/solve_page.ts
@@ -320,4 +320,16 @@ export default function initSolvePage(session: QuizSession) {
         }
         renewProblem();
     })();
+
+    const topbar = document.getElementById('topFixedBar');
+    
+    if (topbar === null) return;
+
+    topbar.style.background = 'inherit';
+    topbar.style.backgroundPositionX = '-55px';
+
+    window.addEventListener('scroll', function() {
+        const position = (25 - window.scrollY) % 75;
+        topbar.style.backgroundPositionY = `${position}px`;
+    });
 }
