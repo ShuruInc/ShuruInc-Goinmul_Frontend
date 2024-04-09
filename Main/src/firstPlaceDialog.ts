6a9e127da9f6bc22f1efd06b8a7c9c0fdbb2f2cb
Cocoa Hoto <cocoa@hoto.us>
2024-04-10 05:42:32 +0900

    feat: first place

--- Main/src/firstPlaceDialog.ts
+++ Main/src/firstPlaceDialog.ts
@@ -12,11 +12,11 @@ export default function createFirstPlaceDialog(
     }">
 
     <h1>1위 달성!</h1>
-    <p class="${styles.congratulations}" style="line-height: 19px;">
-    5월 5일 23시 59분까지<br>1등을 유지하신 분께<br>\"당신의 최애 장르 공식<br>굿즈 10만 원 상당\"을<br> 이벤트 선물로 드립니다! 
-    </p>
-    <p class="${styles.gimmeEmail}">
-    이메일 주소를 기록해주세요<br>
+    <p class="${styles.congratulations}">
+        <span class="${styles.due}">5월 5일 23시 59분까지</span><br>
+        1등을 유지하시면<br>
+        "당신의 최애 공식 굿즈 10만 원 상당"을<br>
+        이벤트 선물로 드립니다!
     </p>
     <form>
         <input type="email" placeholder="example@example.com"><br>
