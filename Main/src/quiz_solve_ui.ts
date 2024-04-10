2a80edd74b0d55c4a6b60a5dc26f0b64e82bc34e
Cocoa Hoto <cocoa@hoto.us>
2024-04-10 19:30:40 +0900

    feat: image

--- Main/src/quiz_solve_ui.ts
+++ Main/src/quiz_solve_ui.ts
@@ -423,10 +423,11 @@ export function updateShareProblem(
     questionEl
         ?.querySelector(".problem-paper-box")
         ?.appendChild(createAnswerElementForShare(question));
+    const paperBox = questionEl?.querySelector(".problem-paper-box");
     const h1 = document.createElement("h1");
     h1.textContent = "친구들아, 도와줘!";
     h1.style.color = '#14171a';
-    questionEl.insertBefore(h1, questionEl.firstChild);
+    paperBox?.insertBefore(h1, paperBox.firstChild);
     root.appendChild(questionEl);
 }
 
